aac533fbd4b1ecff7d99844118cd0b6a813ca622
Amanjot Singh <80406429+gabbar23@users.noreply.github.com>
2024-12-12 16:45:30 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,28 @@
+Time Complexity (TC): O(n * m)
+Space Complexity (SC): O(n)
+
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+      List<Boolean> list = new ArrayList<>();
+
+      for (var q : queries) {
+         int index = 0;
+         boolean flag = true;
+         for (var c : q.toCharArray()) {
+            if(index < pattern.length() && c == pattern.charAt(index)){
+               index++;
+               continue;
+            }
+            if(c >= 'A' && c <= 'Z'){
+               if(index >= pattern.length() || c != pattern.charAt(index)){
+                  flag = false;
+                  break;
+               }
+            }
+         }
+         flag = flag && index == pattern.length();
+         list.add(flag);
+      }
+      return list;
+    }
+}
